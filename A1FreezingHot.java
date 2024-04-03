a240381612c00dc866831c9097c98853beb5357a
Areya <Areya.EmmanuelSingh26@ycdsbk12.ca>
2024-04-02 22:24:00 -0400

    A1FreezingHot - Done

--- A1FreezingHot.java
+++ A1FreezingHot.java
@@ -1,16 +1,20 @@
 class A1FreezingHot extends ConsoleProgram {
 
   /**
-  * Given two temperatures via user input, output true if one is less than 0 and the other is greater than 100.
+  * Given two temperatures via user input, output true if one is less than 0 
+  and the other is greater than 100.
   * @author: Emma Chan  & Areya E-S
   */
   
   public void run() {
-      int temp1; 
-      int temp2; 
+    
+    // Variables
+    int temp1; 
+    int temp2; 
 
-      temp1 = readInt("Tempurature 1: "); 
-      temp2 = readInt ("Tempurature 2: "); 
+    // User Input
+    temp1 = readInt("Tempurature 1: "); 
+    temp2 = readInt ("Tempurature 2: "); 
 
       if ((temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 > 100)){
           System.out.println("True");
